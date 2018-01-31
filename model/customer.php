ed063a20573a8737b3da0244d6ad1396505b26f9
TabibitoRen <nakurakanra@hotmail.com>
2018-01-31 14:47:51 +0000

    unstaged commit on customer

--- model/customer.php
+++ model/customer.php
@@ -3,5 +3,16 @@
     private $id;
     private $givenName;
     private $surname;
+
+    function __construct(){
+    }
+
+    function __get($name){
+      return this->$name;
+    }
+
+    function __set($name,$value){
+      this->$name = $value;
+    }
   }
 ?>
