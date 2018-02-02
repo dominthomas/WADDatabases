1964d7adab49eb2f92d6ca92a8461fabd7c5ae50
Raikume <nakurakanra@hotmail.com>
2018-02-02 13:26:20 +0000

    v0 booking, current uml requests duplicate information

--- model/booking.php
+++ model/booking.php
@@ -1,8 +1,14 @@
 <?php
-  class Customer{
+  class Booking{
     private $id;
-    private $givenName;
-    private $surname;
+    private $Booking_Date;
+    private $Company_ID;
+    private $Flight_No;
+    private $Seats_Booked;
+    private $Customer_ID;
+    private $Cost;
+    private $Aircraft_ID;
+
 
     function __construct(){
     }
