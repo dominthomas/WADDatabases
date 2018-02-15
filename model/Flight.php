72d8b0f018db0a7b60559ab698ca7beb0b2c018f
Raikume <nakurakanra@hotmail.com>
2018-02-15 19:19:46 +0000

    all models completed

--- model/Flight.php
+++ model/Flight.php
@@ -1,8 +1,9 @@
 <?php
   class Flight{
-    private $Flight_No;
+    private $Flight_Number;
     private $Aircraft_ID;
     private $Departure_IATA_Code;
+    private $Departure_Date;
     private $Arrival_IATA_Code;
     private $Arrival_Date;
     private $Cost;
