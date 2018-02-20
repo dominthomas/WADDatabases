de1fae6f600f7b6cbe21fb20882098c79e84d47c
TabibitoRen <nakurakanra@hotmail.com>
2018-02-20 12:03:15 +0000

    Database access completed, neeeds testing

---
+++ model/Database_Access.php
@@ -0,0 +1,54 @@
+<?php
+  $pdo = new PDO("mysql:host=kunet.kingston.ac.uk;dbname=db_k1507061",
+  "k1507061",
+  "Ouzen~1",
+  [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+  );
+
+  function getAllAircraft_Models(){
+    global $pdo;
+    $statement = $pdo->prepare("SELECT * FROM Aircrafts");
+    $statement->execute();
+    $results = $statement->fetchAll(PDO::FETCH_CLASS,"Aircraft");
+    return $results;
+  }
+
+  function getAllAircrafts(){
+    global $pdo;
+    $statement = $pdo->prepare("SELECT * FROM Aircrafts");
+    $statement->execute();
+    $results = $statement->fetchAll(PDO::FETCH_CLASS,"Aircraft");
+    return $results;
+  }
+
+  function getAllBookings(){
+    global $pdo;
+    $statement = $pdo->prepare("SELECT * FROM Aircrafts");
+    $statement->execute();
+    $results = $statement->fetchAll(PDO::FETCH_CLASS,"Aircraft");
+    return $results;
+  }
+
+  function getAllCompanies(){
+    global $pdo;
+    $statement = $pdo->prepare("SELECT * FROM Fly_Guys");
+    $statement->execute();
+    $results = $statement->fetchAll(PDO::FETCH_CLASS,"Company");
+    return $results;
+  }
+  function getAllDestinations(){
+    global $pdo;
+    $statement = $pdo->prepare("SELECT * FROM Destinations");
+    $statement->execute();
+    $results = $statement->fetchAll(PDO::FETCH_CLASS,"Destination");
+    return $results;
+  }
+
+  function getAllFlights(){
+    global $pdo;
+    $statement = $pdo->prepare("SELECT * FROM Flights");
+    $statement->execute();
+    $results = $statement->fetchAll(PDO::FETCH_CLASS,"Flight");
+    return $results;
+  }
+?>
