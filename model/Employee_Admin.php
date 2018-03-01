bc46d37b32f3a236a16ccc49141ee9aabfcd30cc
TabibitoRen <nakurakanra@hotmail.com>
2018-03-01 15:53:09 +0000

    Employee Admin account

---
+++ model/Employee_Admin.php
@@ -0,0 +1,18 @@
+<?php
+  class Employee_Admin{
+    private $Admin_ID;
+    private $Email_Address;
+    private $Password;
+
+    function __construct(){
+    }
+
+    function __get($name){
+      return this->$name;
+    }
+
+    function __set($name,$value){
+      this->$name = $value;
+    }
+  }
+?>
