b273eafac9c11fc1fb77ff3646cade91c170ca24
Domin Thomas <k1651915@kingston.ac.uk>
2018-03-30 17:46:33 +0100

    airportList changed from singleton method

--- controller/airportList.php
+++ controller/airportList.php
@@ -2,5 +2,5 @@
 require_once "../model/Airport.php";
 require_once "../model/Database_Access.php";
 
-$airportList = Database_Access::getInstance()->getAllAirports();
+$airportList = getAllAirports();
 ?>
