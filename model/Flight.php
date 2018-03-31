dd832db729a4a2d3276a530f8898ac9368ebda68
Domin Thomas <k1651915@kingston.ac.uk>
2018-03-31 17:00:11 +0100

    saved flight.php

--- model/Flight.php
+++ model/Flight.php
@@ -5,12 +5,9 @@
     private $Departure_IATA_Code;
     private $Departure_Date;
     private $Arrival_IATA_Code;
-    private $Arrival_Date;
     private $Cost;
-    private $Seats_Booked;
     private $Departure_Time;
-    private $Arrival_Time;
-
+    private $Flight_Duration;
     function __construct(){
     }
 
@@ -20,13 +17,6 @@
 
     function __set($name,$value){
       $this->$name = $value;
-    }
-
-    function journey(){
-      $start = explode(":", $this->Departure_Time);
-      $end = explode(":", $this->Arrival_Time);
-      $journey = $start[0].":".$start[1]."-".$end[0].":".$end[1];
-      return $journey;
     }
   }
 ?>
