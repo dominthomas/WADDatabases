f204d0bc0ea96173e1c1ebcaa703ccc7f71ac5ac
Raikume <nakurakanra@hotmail.com>
2018-04-01 19:09:44 +0100

    something

--- controller/basketController.php
+++ controller/basketController.php
@@ -1,11 +1,13 @@
 <?php
 require_once "../model/Basket.php";
+require_once "../model/Flight.php";
 
 $basket = new Basket();
 
 if(isset($_REQUEST['addBasket'])){
   $basket->addFlight($_REQUEST['addBasket']);
 }
-
-
+elseif(isset($_REQUEST['removeBasket'])){
+  $basket->removeFlight($_REQUEST['removeBasket']);
+}
 ?>
