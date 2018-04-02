a5e591712bf4e47df8defa58848e04e9678ffe60
Raikume <nakurakanra@hotmail.com>
2018-04-02 22:05:04 +0100

    customer login added, needs testing

--- controller/customerController.php
+++ controller/customerController.php
@@ -1,11 +1,19 @@
 <?php
 require_once "../model/Database_Access.php"
+require_once "../model/Customer.php"
 
 /*Pass customer object*/
-if(isset($_REQUEST['login'])){
-  if(Database_Access::checkLogin($_REQUEST['login'])){
-    $customer = Database_Access::checkLogin($_REQUEST['login'];
+if(isset($_REQUEST['email'])){
+  $customer = new Customer();
+  $customer->Email_Address = $_REQUEST['email'];
+  $customer->Password = $_REQUEST['password'];
+  if(Database_Access::checkLogin($customer)){
+    $customer = Database_Access::checkLogin($customer];
     $_SESSION['user'] = $customer;
+    echo "You have logged in successfully";
+  }
+  else{
+    echo "Your password or email is incorrect";
   }
 }
 ?>
---
+++ view/customerLogin.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>Customer Login</title>
+  </head>
+  <body>
+    <form action="../controller/customerController.php">
+      <input type="text" name="username"/>
+      <input type="text" name="password"/>
+      <input type="submit" value="login"/>
+    </form>
+  </body>
+</html>
