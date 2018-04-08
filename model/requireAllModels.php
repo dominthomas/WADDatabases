edbdf2a1f4c59da0e581477f7198a254ca9bdd8a
Domin Thomas <k1651915@kingston.ac.uk>
2018-04-08 23:09:14 +0100

    ALBERT IF YOU MERGER AGAIN I WILL KILL YOU. AND STOP USING COMMAND LINE GIT. You can't handle the power of the penguin yet.

--- model/requireAllModels.php
+++ model/requireAllModels.php
@@ -3,7 +3,6 @@
   require_once "../model/Aircraft.php";
   require_once "../model/Airport.php";
   require_once "../model/Booking.php";
-  require_once "../model/Customer.php";
   require_once "../model/Flight.php";
   require_once "../model/AdminKey.php";
   require_once "../model/Admin.php";
